1dcc42fe66bba82abbfea2113c8714d5d3b9375d
Rock Shen <rock_shen@asus.com>
2021-06-18 21:29:11 +0800

    Fix dtoverlay.img not pack to update.img
    Change-Id: I5817cbfad1ecef69e9679d438925968d4cc7e3be

--- linux/Linux_Pack_Firmware/rockdev/mkupdate_WW_Tinker_Board_2.sh
+++ linux/Linux_Pack_Firmware/rockdev/mkupdate_WW_Tinker_Board_2.sh
@@ -14,8 +14,16 @@ if [ ! -f "package-file-Tinker_Board_2" ]; then
 	echo "Error:No found package-file!"
 #	pause
 fi
+
+mv package-file package-file.bak
+cp package-file-Tinker_Board_2 package-file
+
 ./afptool -pack ./ Image/update.img || pause
 ./rkImageMaker -RK330C Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
+
+rm package-file
+mv package-file.bak package-file
+
 echo "Making update.img OK."
 #echo "Press any key to quit:"
 #read -n1 -s key
